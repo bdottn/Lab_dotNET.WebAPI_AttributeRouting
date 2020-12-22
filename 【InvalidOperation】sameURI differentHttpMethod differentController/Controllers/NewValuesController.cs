4414c1dc8a5209ce1bbf323a96715f1b3fffd6bb
Echo Lee <bdottn.lee+git@gmail.com>
2020-12-22 11:43:38 +0800

    環境模擬
    1. 建立兩個 Web API 2 空白控制器：ValuesController、NewValuesController。
    2. 在兩個 Controller 上各自建立一個方法。
    3. 在這兩個方法掛上不同的 HttpMethod。
    4. 在這兩個方法使用屬性路由掛上相同的 URI。

---
+++ 【InvalidOperation】sameURI differentHttpMethod differentController/Controllers/NewValuesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace InvalidOperation_sameURI_differentHttpMethod_differentController.Controllers
+{
+    public sealed class NewValuesController : ApiController
+    {
+        [HttpGet, Route("values/{id:int}")]
+        public string GetValue(int id)
+        {
+            return "value is " + id;
+        }
+    }
+}
---
+++ 【InvalidOperation】sameURI differentHttpMethod differentController/Controllers/ValuesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace InvalidOperation_sameURI_differentHttpMethod_differentController.Controllers
+{
+    public sealed class ValuesController : ApiController
+    {
+        [HttpDelete, Route("values/{id:int}")]
+        public string DeleteValue(int id)
+        {
+            return "delete value success !";
+        }
+    }
+}
